164981d55bbc82af650a2fbc3734e2913e47d88f
Dustin Arnold <dustin.arnold@compassphs.com>
2016-12-04 21:32:37 -0600

    added solutions for day 2

---
+++ dotnet/day2/Day2Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace dotnet.day2
+{
+    [TestClass]
+    public class Day2Tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var solver = new Solver(new NormalKeypadLayout());
+
+            var expected = "1985";
+            var input = new string[] { "ULL", "RRDDD", "LURDL", "UUUD" };
+
+            var actual = solver.GetCodeFromInput(input, "5");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var path = string.Format("\\\\Mac\\Home\\Documents\\Projects\\Sandbox\\AdventOfCode2016\\dotnet\\day2\\input.txt");
+            var input = File.ReadAllLines(path);
+
+            var solver = new Solver(new NormalKeypadLayout());
+            var expected = "92435";
+
+            var actual = solver.GetCodeFromInput(input, "5");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Part2()
+        {
+            var path = string.Format("\\\\Mac\\Home\\Documents\\Projects\\Sandbox\\AdventOfCode2016\\dotnet\\day2\\input.txt");
+            var input = File.ReadAllLines(path);
+
+            var solver = new Solver(new AdvancedKeypadLayout());
+            var expected = "C1A88";
+
+            var actual = solver.GetCodeFromInput(input, "5");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ dotnet/day2/Solver.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet.day2
+{
+    public interface IKeypadLayout
+    {
+        IEnumerable<IEnumerable<string>> GetButtons();
+    }
+
+    public class NormalKeypadLayout : IKeypadLayout
+    {
+        public IEnumerable<IEnumerable<string>> GetButtons()
+        {
+            var list = new List<List<string>>();
+
+            list.Add(new List<string>(new string[] { "1", "2", "3" }));
+            list.Add(new List<string>(new string[] { "4", "5", "6" }));
+            list.Add(new List<string>(new string[] { "7", "8", "9" }));
+
+            return list;
+        }
+    }
+    public class AdvancedKeypadLayout : IKeypadLayout
+    {
+        public IEnumerable<IEnumerable<string>> GetButtons()
+        {
+            var list = new List<List<string>>();
+
+            list.Add(new List<string>(new string[] { " ", " ", "1", " ", " " }));
+            list.Add(new List<string>(new string[] { " ", "2", "3", "4", " " }));
+            list.Add(new List<string>(new string[] { "5", "6", "7", "8", "9" }));
+            list.Add(new List<string>(new string[] { " ", "A", "B", "C", " " }));
+            list.Add(new List<string>(new string[] { " ", " ", "D", " ", " " }));
+
+            return list;
+        }
+    }
+
+    public class Solver
+    {
+        readonly List<Tuple<string, int, int>> _keypad;
+
+        public Solver(IKeypadLayout keypadLayout)
+        {
+            _keypad = new List<Tuple<string, int, int>>();
+
+            var x = 0;
+            var y = 0;
+
+            foreach (var row in keypadLayout.GetButtons())
+            {
+                foreach (var col in row)
+                {
+                    _keypad.Add(new Tuple<string, int, int>(col, x, y));
+                    y++;
+                }
+
+                x++;
+                y = 0;
+            }
+        }
+
+        public string GetCodeFromInput(IEnumerable<string> input, string startingButton)
+        {
+            var sb = new StringBuilder();
+
+            var currentKey = _keypad.Single(k => k.Item1 == startingButton);
+
+            foreach (var line in input)
+            {
+                foreach (var move in line)
+                    currentKey = GetAdjacentKey(currentKey.Item2, currentKey.Item3, move) ?? currentKey;
+
+                sb.Append(currentKey.Item1);
+            }
+
+            return sb.ToString();
+        }
+
+        Tuple<string, int, int> GetAdjacentKey(int x, int y, char direction)
+        {
+            switch (direction)
+            {
+                case 'L':
+                    return _keypad.FirstOrDefault(k => !string.IsNullOrWhiteSpace(k.Item1) && k.Item2 == x && k.Item3 == y - 1);
+
+                case 'R':
+                    return _keypad.FirstOrDefault(k => !string.IsNullOrWhiteSpace(k.Item1) && k.Item2 == x && k.Item3 == y + 1);
+
+                case 'U':
+                    return _keypad.FirstOrDefault(k => !string.IsNullOrWhiteSpace(k.Item1) && k.Item2 == x - 1 && k.Item3 == y);
+
+                case 'D':
+                    return _keypad.FirstOrDefault(k => !string.IsNullOrWhiteSpace(k.Item1) && k.Item2 == x + 1 && k.Item3 == y);
+            }
+
+            return null;
+        }
+    }
+}
