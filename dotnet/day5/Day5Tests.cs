79e1e9665129ee648cc2d892585a70e0338da768
Dustin Arnold <dustin.arnold@compassphs.com>
2016-12-09 10:13:44 -0600

    added day 5 part 1

---
+++ dotnet/day5/Day5Tests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace dotnet.day5
+{
+    [TestClass]
+    public class Day4Tests
+    {
+        [TestMethod]
+        public void ExamplePart1()
+        {
+            var solver = new Solver();
+
+            var expected = 3231929;
+            var actual = solver.GetFirstHashStartingWithFiveZeroes("abc", 3231928);
+
+            Assert.AreEqual(expected, actual.Item1);
+
+            expected = 5017308;
+            actual = solver.GetFirstHashStartingWithFiveZeroes("abc", 5017300);
+
+            Assert.AreEqual(expected, actual.Item1);
+
+            expected = 5278568;
+            actual = solver.GetFirstHashStartingWithFiveZeroes("abc", 5278560);
+
+            Assert.AreEqual(expected, actual.Item1);
+        }
+        [TestMethod]
+        public void ExamplePart2()
+        {
+            var solver = new Solver();
+
+            var startingIndex = 3231928;
+            var expected = "18F";
+
+            var input = new string[] { "abc" };
+
+            var actual = solver.GetDoorPassword(startingIndex, input[0], 3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            var solver = new Solver();
+
+            var startingIndex = 3231928;
+            var expected = "18F47A30";
+
+            var input = new string[] { "abc" };
+
+            var actual = solver.GetDoorPassword(startingIndex, input[0], 8);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var path = string.Format("\\\\Mac\\Home\\Documents\\Projects\\Sandbox\\AdventOfCode2016\\dotnet\\day5\\input.txt");
+            var input = File.ReadAllLines(path);
+
+            var solver = new Solver();
+            var expected = "A";
+
+            var actual = solver.GetDoorPassword(0, input[0], 8);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[TestMethod]
+        //public void Example2()
+        //{
+        //    var solver = new Solver();
+
+        //    var expected = "very encrypted name";
+
+        //    var encryptedname = "qzmt-zixmtkozy-ivhz";
+        //    var sectorid = 343;
+
+        //    var actual = solver.DecipherName(encryptedname,sectorid);
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+        //[TestMethod]
+        //public void Part2()
+        //{
+        //    var path = string.Format("\\\\Mac\\Home\\Documents\\Projects\\Sandbox\\AdventOfCode2016\\dotnet\\day4\\input.txt");
+        //    var input = File.ReadAllLines(path);
+
+        //    var solver = new Solver();
+        //    var expected = 548;
+
+        //    var actual = solver.GetSectorIdOfRoomWithNorthPoleObjects(input);
+        //    Assert.AreEqual(expected, actual);
+        //}
+    }
+}
---
+++ dotnet/day5/Solver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace dotnet.day5
+{
+    public class Solver
+    {
+        public string GetDoorPassword(int startingIndex, string doorId, int passwordLength)
+        {
+            var sb = new StringBuilder();
+
+            var currentIndex = startingIndex;
+
+            while (sb.Length < passwordLength)
+            {
+                var nextResult = GetFirstHashStartingWithFiveZeroes(doorId, currentIndex);
+
+                sb.Append(nextResult.Item2[5]);
+
+                currentIndex = nextResult.Item1 + 1;
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns the index and the hash
+        /// </summary>
+        public Tuple<int, string> GetFirstHashStartingWithFiveZeroes(string s, int startingIndex)
+        {
+            var currentIndex = startingIndex;
+
+            while (true)
+            {
+                using (var md5 = MD5.Create())
+                {
+                    var input = System.Text.Encoding.ASCII.GetBytes(s + currentIndex.ToString());
+                    var hash = md5.ComputeHash(input);
+
+                    var sb = new StringBuilder();
+                    for (int i = 0; i < hash.Length; i++)
+                    {
+                        sb.Append(hash[i].ToString("X2"));
+                    }
+
+                    var result = sb.ToString();
+
+                    if (result.StartsWith("00000"))
+                        return new Tuple<int, string>(currentIndex, result);
+
+                    currentIndex++;
+                }
+            }
+        }
+    }
+}
